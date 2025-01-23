e5a721f3d5e8f191382e61bfdaf54f36df1020b8
nyambogahezron <hezzyn1@gmail.com>
2025-01-23 21:30:56 +0300

    Add initial project setup with navigation and home screen
    - Create a funny quiz app structure
    - Implement navigation using React Navigation
    - Add HomeScreen component with Lottie animation
    - Include necessary dependencies in package.json
    - Configure Metro and SQLite settings
    - Update .gitignore for local environment files

---
+++ src/screen/HomeScreen.tsx
@@ -0,0 +1,52 @@
+import { useRef, useEffect } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+import * as Network from 'expo-network';
+
+export default function HomeScreen() {
+  const animation = useRef<LottieView>(null);
+  useEffect(() => {
+    // You can control the ref programmatically, rather than using autoPlay
+    // animation.current?.play();
+  }, []);
+
+  const networkState = Network.useNetworkState();
+  console.log(`Current network type: ${networkState.type}`);
+
+  return (
+    <View style={styles.animationContainer}>
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: '#eee',
+        }}
+        // Find more Lottie files at https://lottiefiles.com/featured
+        source={require('../../assets/gradientBall.json')}
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+          title='Restart Animation'
+          onPress={() => {
+            animation.current?.reset();
+            animation.current?.play();
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  animationContainer: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
+  },
+});
